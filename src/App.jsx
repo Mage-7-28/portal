62df6f5672918a0e0f6088de78da0b783dc6969d
cherry <liusir918@163.com>
2026-03-17 20:46:47 +0800

    添加字体样式

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ConfigProvider, theme } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
-import { GlobalFontFamily } from "./utils/GlobalEnum.js";
+import { GlobalFontFamily } from './utils/GlobalEnum.js'
 
 function App() {
 
@@ -22,7 +22,7 @@ function App() {
         }
       }
     } } locale={ zhCN } componentSize={ 'middle' }>
-111
+你是谁
     </ConfigProvider>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 // 引入样式文件
 import './style/index.css'
 import './style/fonts.css'
+import { GlobalFontFamily } from './utils/GlobalEnum.js'
+
+// 设置CSS变量
+document.documentElement.style.setProperty('--global-font-family', GlobalFontFamily)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
