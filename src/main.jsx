9eaab7f8344512b6bddf92d34b6e0afbf768cc30
cherry <liusir918@163.com>
2026-03-22 00:25:10 +0800

    添加应用加载动画

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,17 @@ import { GlobalFontFamily } from './utils/GlobalEnum.js'
 // 设置CSS变量
 document.documentElement.style.setProperty('--global-font-family', GlobalFontFamily)
 
+// 渲染应用
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+// 应用加载完成后隐藏加载动画
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.getElementById('loading').style.display = 'none'
+    document.getElementById('root').style.display = 'block'
+  }, 200) // 增加一个小延迟，确保加载动画有足够的时间显示
+})
