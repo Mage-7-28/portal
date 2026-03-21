ecf42411801863b8d32e080c2dc655a943f5cfca
cherry <liusir918@163.com>
2026-03-22 01:16:37 +0800

    修改群岛样式

--- src/components/Remote.jsx
+++ src/components/Remote.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Remote() {
   return (
-    <div style={{ height: '100%', padding: '20px', backgroundColor: '#1e1e1e', borderRadius: '4px' }}>
-      <h3>远程文件</h3>
-      <p>远程文件浏览器组件</p>
+    <div style={{ borderRadius: '10px', backgroundColor: '#101113', padding: 10 }}>
+      <span>远程文件</span>
+      <span>远程文件浏览器组件</span>
     </div>
   )
 }
